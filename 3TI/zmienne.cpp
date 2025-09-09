22b34aa519e5b135b35991d86100ff10bbada417
jawlo6 <januszwlodarz@gmail.com>
2025-09-09 15:02:36 +0200

    9.9.2025

---
+++ 3TI/zmienne.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int liczba=10;
+    double liczba2=4.2121;
+    char znak='a';
+    string imie="Karol";
+    bool znaleziono = true;
+
+    cout << liczba << "\n";
+    cout << liczba2 << "\n";
+    cout << znak << "\n" << imie << "\n";
+    cout << znaleziono;
+}

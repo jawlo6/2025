5961cdd726738de98527400735c89c277d9dd222
jawlo6 <januszwlodarz@gmail.com>
2025-12-02 15:02:00 +0100

    2.12.2025

---
+++ 3TI2/tablica_kopia.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+// kopiowanie elementów tablicy do drugiej tablicy
+
+int main () {
+
+    int tablica[5]={1,2,3,4,5};  //deklaracja tablicy 10elementów
+    int kopia[5];
+
+    for( int i=0; i<5; i++ ){
+        kopia[i]=tablica[i];   // kopiowanie kolejnego elementu
+    }
+    // wypisz skopiowaną tablicę
+    for( int i=0; i<5; i++ ){
+        cout << kopia[i] << "\n";
+    }
+}
